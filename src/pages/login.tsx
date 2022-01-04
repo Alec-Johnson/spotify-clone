4f2a4039e98b3cf7a76a458be9a1749720951927
Alec Johnson <akj3211@gmail.com>
2022-01-04 14:33:24 -0600

    Login functionality technically working

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,7 +3,17 @@ import { getProviders, signIn} from 'next-auth/react'
 import Image from 'next/image';
 import styles from '../styles/Login.module.scss'
 
-type SpotifyProviderProps = {}
+type SpotifyProviderProps = {
+  providers: {
+    spotify: {
+      callBackUrl: string,
+      id: string,
+      name: string,
+      signInUrl: string, 
+      type: string,
+    }
+  }
+}
 
 function Login({ providers }: SpotifyProviderProps) {
   console.log(providers);
@@ -11,9 +21,24 @@ function Login({ providers }: SpotifyProviderProps) {
   return (
     <div className={styles.container}>
       <Image src='/spotify-logo.png' width={300} height={300} alt='Spotify logo' />
-      
+      {/* Object.values() returns an array of the values of the object, .entries() returns an array of the key-value pairs of the object */}
+      {Object.values(providers).map((spotify) => ( 
+        <div key={spotify.name}>
+          <button className={styles.loginBtn} onClick={() => signIn(spotify.id, { callbackUrl: "/"})}>Login with {spotify.name}</button>
+        </div>
+      ))} 
     </div>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const providers = await getProviders()
+
+  return {
+    props: {
+      providers,
+    },
+  };
 }
 
 export default Login;
