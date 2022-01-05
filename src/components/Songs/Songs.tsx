7ea0ebb49475c74e4c4b3a438e30f6100d49f221
Alec Johnson <akj3211@gmail.com>
2022-01-05 12:34:46 -0600

    Created Songs and Song components

---
+++ src/components/Songs/Songs.tsx
@@ -0,0 +1,18 @@
+import Song from "@components/Song/Song"
+import { useRecoilValue } from "recoil"
+import { playlistState } from "src/atoms/playlistAtom"
+import styles from './Songs.module.scss'
+function Songs() {
+  // Already have the playlist
+  const playlist = useRecoilValue(playlistState)
+
+  return (
+    <div className={styles.container}>
+      {playlist?.tracks.items.map((track, i) => (
+        <Song key={track.track.id} track={track} order={i} />
+      ))}
+    </div>
+  )
+}
+
+export default Songs
