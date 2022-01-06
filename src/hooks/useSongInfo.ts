30a2157b2621f479616ee55a208f50d6915bbb1f
Alec Johnson <akj3211@gmail.com>
2022-01-05 19:31:18 -0600

    useSongInfo custom hook

---
+++ src/hooks/useSongInfo.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { currentTrackIdState } from "src/atoms/songAtom";
+import useSpotify from "./useSpotify";
+
+function useSongInfo() {
+	const spotifyApi = useSpotify();
+	const [currentIdTrack, setCurrentIdTrack] =
+		useRecoilState(currentTrackIdState);
+	const [songInfo, setSongInfo] = useState<any | null>(null);
+
+	// Async is not supported in useEffect by default, so we need to define the function, then call it within useEffect
+	useEffect(() => {
+		const fetchSongInfo = async () => {
+			if (currentIdTrack) {
+				const track = await spotifyApi.getTrack(currentIdTrack);
+				setSongInfo(track);
+			}
+		};
+		fetchSongInfo();
+	});
+
+	return songInfo;
+}
+
+export default useSongInfo;
