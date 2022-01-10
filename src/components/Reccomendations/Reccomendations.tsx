d7e22a593051455616e14360f6fabc95dc65bdaa
Alec Johnson <akj3211@gmail.com>
2022-01-10 11:43:14 -0600

    Reccomendation component for feed view

---
+++ src/components/Reccomendations/Reccomendations.tsx
@@ -0,0 +1,24 @@
+import { useRecoilValue } from 'recoil';
+import { reccomendationsState } from 'src/atoms/feedAtom';
+import styles from './Reccomendations.module.scss'
+
+function Reccomendations() {
+  const reccomended = useRecoilValue(reccomendationsState);
+ 
+  return (
+    <div className={styles.container}>
+      <h2>For You</h2>
+      {reccomended && reccomended.tracks.map((track: SpotifyApi.TrackObjectFull) => (
+        <div key={track.id} className={styles.track}>
+          <img src={track.album.images[1].url} alt={track.album.name} />
+          <div className={styles.artistWrapper}>
+            <div className={styles.trackName}>{track.name}</div>
+            <hr />
+            <div className={styles.trackArtist}>{track.artists[0].name}</div></div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Reccomendations
